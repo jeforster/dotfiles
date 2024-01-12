33f10f09e85caca85ed5de2e9a96868c6541b817
Jeffrey Forster <jforster@microsoft.com>
2024-01-12 13:00:50 -0500

    remove old powershell PSReadline module versions on windows

--- dotfiles/config/powershell/platforms/windows.ps1
+++ dotfiles/config/powershell/platforms/windows.ps1
@@ -3,3 +3,9 @@ if (!(Get-Command "oh-my-posh" -errorAction SilentlyContinue))
     Set-ExecutionPolicy Bypass -Scope Process -Force; Invoke-Expression ((New-Object System.Net.WebClient).DownloadString("https://ohmyposh.dev/install.ps1"))
     (Get-Command oh-my-posh).Source
 }
+
+# Remove old system versions of PSReadline
+$psReadlinePath = "${Env:Programfiles}\WindowsPowerShell\Modules\PSReadLine"
+if (Test-Path $path) {
+    Remove-Item -Path $psReadlinePath -Recurse -Force
+}
